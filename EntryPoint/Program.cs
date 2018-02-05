fcb369388daf9ca4c2f87fea8e46051b30923056
Ahmed Jardak <ahmed.jardak@hotmail.com>
2018-02-05 22:22:10 +0100

    FOP

---
+++ EntryPoint/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FOP;
+
+namespace EntryPoint
+{
+    public static class  Program
+    {
+        static void Main(string[] args)
+        {
+            Util.GeneratePdf("fonts.fo.xml", "fonts.fo.pdf");
+            Util.GenerateXmlFromXslt("NameFo.xsl","Name.xml","Name.fo.xml");
+            Util.GeneratePdf("Name.fo.xml", "Name.fo.pdf");
+            Console.Read();
+        }
+    }
+}
---
+++ FOP/Util.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+using org.apache.fop.tools;
+using org.apache.fop.apps;
+using org.xml.sax;
+using java.io;
+
+namespace FOP
+{
+  
+  public static class Util
+    {
+        public static void GenerateXmlFromXslt(string xsltFilePath,string xmlfilePath,string resultFilePath)
+        {
+            var myXslTrans = new XslCompiledTransform();
+            myXslTrans.Load(xsltFilePath);
+            myXslTrans.Transform(xmlfilePath, resultFilePath);
+        }
+        public static void GeneratePdf(string foFile, string pdfFile)
+        {
+            OutputStream os = new BufferedOutputStream(new FileOutputStream(new java.io.File(pdfFile)));
+
+            try
+            {
+                FopFactory fopFactory = FopFactory.newInstance(new java.net.URI("."));
+                Fop fop = fopFactory.newFop("application/pdf", os);
+                javax.xml.transform.TransformerFactory factory = javax.xml.transform.TransformerFactory.newInstance();
+                javax.xml.transform.Transformer transformer = factory.newTransformer();
+                javax.xml.transform.Source source = new javax.xml.transform.stream.StreamSource(new java.io.File(foFile));
+                javax.xml.transform.Result result = new javax.xml.transform.sax.SAXResult(fop.getDefaultHandler());
+                transformer.transform(source, result);
+            }
+
+            finally
+            {
+                os.close();
+            }
+        }
+    }
+}
